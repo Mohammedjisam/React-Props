5a8684b707a031a4c88707ce3385f2750424c242
Mohammed Jisam <66182731+Mohammedjisam@users.noreply.github.com>
2024-09-02 10:17:50 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+
+import Student from "./Student";
+
+
+export default function App() {
+  return ( 
+    <>
+    <Student name="jisam" age={30} isStudent={true}/>
+    <Student name="adith" age={42} isStudent={false}/>
+    <Student name="abhishek" age={84} isStudent={false}/>
+    <Student name="Aslam" age={49} isStudent={true}/>
+    <Student />
+    </>
+          
+  
+  
+  )
+}
